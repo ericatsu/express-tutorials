7fc066634ac0d2312359fb4601bc465b5840f4d5
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-27 15:42:16 +0000

    reading all files using just html

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const http = require('http');
 
 
 //Reading all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const homeLogo = readFileSync('./navbar-app/logo.svg')
 
 const server = http.createServer((req, res) => {
     const url = req.url;
@@ -11,11 +14,20 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.write(homePage)
         res.end()
-    } else if(url === '/about'){
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.write(homePage)
+    } else if(url === '/styles.css'){
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+        res.write(homeStyles)
+        res.end()
+    } else if (url === '/logo.svg') {
+        res.writeHead(200, { 'Content-Type': 'text/svg+xml' })
+        res.write(homeLogo)
         res.end()
     }
+    else if (url === '/browser-app.js') {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' })
+        res.write(homeLogic)
+        res.end()
+    } 
     else {
         res.writeHead(404, { 'Content-Type': 'text/html' })
         res.write('<h1>Page Not Found</h1>')
