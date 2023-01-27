3589798dc44bc5c4697f869b3b3e3f09211ee319
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-27 14:33:04 +0000

    reading html files

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const http = require('http')
+const { readFileSync } = require('fs');
+const http = require('http');
+
+
+//Reading all files
+const homePage = readFileSync('./index.html')
 
 const server = http.createServer((req, res) => {
     const url = req.url;
     if (url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.write('<h1>Home Page</h1>')
+        res.write(homePage)
         res.end()
     } else if(url === '/about'){
         res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.write('<h1>About Page</h1>')
+        res.write(homePage)
         res.end()
     }
     else {
