af706fd3d563a3fc309e97e59d68db2917e2c11f
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-27 13:39:56 +0000

    using status codes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('<h1>Home Page</h1>')
-    res.end()
+    const url = req.url;
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    } else if(url === '/about'){
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
