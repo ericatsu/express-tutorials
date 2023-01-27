13b15af7fc6fedf98849a5dfd00bdce27b76c072
ericatsu <ekaatsu@st.ug.edu.gh>
2023-01-27 17:08:47 +0000

    creating express server

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
-const { readFileSync } = require('fs');
-const http = require('http');
+const express = require('express')
+const app = express()
 
-
-//Reading all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-const homeLogo = readFileSync('./navbar-app/logo.svg')
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.write(homePage)
-        res.end()
-    } else if(url === '/styles.css'){
-        res.writeHead(200, { 'Content-Type': 'text/css' })
-        res.write(homeStyles)
-        res.end()
-    } else if (url === '/logo.svg') {
-        res.writeHead(200, { 'Content-Type': 'text/svg+xml' })
-        res.write(homeLogo)
-        res.end()
-    }
-    else if (url === '/browser-app.js') {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' })
-        res.write(homeLogic)
-        res.end()
-    } 
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        res.write('<h1>Page Not Found</h1>')
-        res.end()
-    }
+app.get('/', (req, res) => {
+    res.status(200).end('The Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+    res.status(200).end('The About Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).end('<h1>Page Not Found</h1>')
+})
+app.listen(5000, () => {
+    console.log('Server listening on 5000');
+})
---
+++ http-app.js
@@ -0,0 +1,38 @@
+const { readFileSync } = require('fs');
+const http = require('http');
+
+
+//Reading all files
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const homeLogo = readFileSync('./navbar-app/logo.svg')
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(homePage)
+        res.end()
+    } else if (url === '/styles.css') {
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+        res.write(homeStyles)
+        res.end()
+    } else if (url === '/logo.svg') {
+        res.writeHead(200, { 'Content-Type': 'text/svg+xml' })
+        res.write(homeLogo)
+        res.end()
+    }
+    else if (url === '/browser-app.js') {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' })
+        res.write(homeLogic)
+        res.end()
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000)
