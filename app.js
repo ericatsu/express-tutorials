78c89dc0cce9c5e6e0c6861971234974ce65a9bb
ericatsu <ekaatsu@st.ug.edu.gh>
2023-02-02 10:45:54 +0000

    setup static and middleware : 5:10

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express();
 
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.end('Home Page')
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
