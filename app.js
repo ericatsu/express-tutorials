775bff0fd6c7fce3a72dbce64d64ca9f2bf8a4ca
ericatsu <ekaatsu@st.ug.edu.gh>
2023-02-16 21:22:01 +0000

    🚑

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json([{name: 'Eric'}, {name: 'Atsu'}])
+    res.json([{name: 'Eric'}, {name: 'Atsu z'}])
 })
 
 app.listen(5000, () => {
