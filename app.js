84cae5253557ea3789495b5741f2a44c20599c1c
ericatsu <ekaatsu@st.ug.edu.gh>
2023-02-24 12:10:52 +0000

    returing specific properties

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express();
 const {products} = require('./data')
 
 app.get('/', (req, res) => {
-    res.send('<h1>HomePage</h1> <a href="./api/products"> Products </a>')
+    res.send('<h1>HomePage</h1> <a href="/api/products"> Products </a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name, image}
+    });
+
+    res.json(newProducts)
 })
 
 app.listen(5000, () => {
